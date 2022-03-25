05231a68dcbecbf095e259a92bf0ad427d6667a5
mahmood seoud <Mahmoodseoud@gmail.com>
2022-03-25 18:35:09 +0100

    Second push properly Final

---
+++ appointmentMaker/appointmentMaker/Controllers/appoitmentController.cs
@@ -0,0 +1,26 @@
+﻿using appointmentMaker.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace appointmentMaker.Controllers
+{
+    public class appoitmentController : Controller
+    {
+        static List<appointmentModel> appointments = new List<appointmentModel>();
+        public IActionResult Index()
+        {
+            return View(appointments);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Details(appointmentModel appointment)
+        {
+            appointments.Add(appointment);
+            return View("Details", appointment);
+        }
+    }
+}
---
+++ appointmentMaker/appointmentMaker/Models/appointmentModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace appointmentMaker.Models
+{
+    public class appointmentModel
+    {   [Required]
+        [StringLength(20, MinimumLength = 4)]
+        [DisplayName("Patient's Full Name")]
+        public string PatientName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayName("Appointment Request Date")]
+        public DateTime AppointmentDate { get; set; }
+
+        [DataType(DataType.Currency)]
+        [DisplayName("Patient's Approximate Net Worth")]
+        public decimal PatientNetWorth { get; set; }
+
+
+        [Required]
+        [DisplayName("Doctors's Full Name")]
+        public string DocterName { get; set; }
+
+        [Range(1,10)]
+        [DisplayName("Patient's Percieved Level of Pain (1 low to 10 high)")]
+        public int PainLvl { get; set; }
+
+    }
+}
